296fc8de05f06100417663d87c8a917be9a8fd4d
evelynriandrade6 <eveyandrade0.3@gmail.com>
2024-11-25 15:32:55 -0300

    25_11 atualizacao

--- front/appEvento.js
+++ front/appEvento.js
@@ -1,7 +1,20 @@
+// Chamada da função "testeCalendario" para associação ao evento de envio do formulário
 document
 .getElementById("form-selecao-data")
 .addEventListener("submit", testeCalendario);
 
 function testeCalendario(event){
-    
+  // Previne o comportamento padrão do formulário, ou seja, impede que ele seja enviado e recarregue a página
+  event.preventDefault();
+
+  const data_recebida = document.getElementById("data").value;
+
+  if(data_recebida){
+      // console.log("Data recebida: ", data_recebida);
+  alert("A data selecionada é: " + data_recebida);
+  }
+  else{
+    alert("Por favor, selecione uma data!")
+  }
+
 }
